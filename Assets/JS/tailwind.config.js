3ceabde9a79e10d6834700e3d9d2296da639f90a
fire8327 <114702513+fire8327@users.noreply.github.com>
2023-01-07 13:55:31 +0000

    create header adn 1st block

--- Assets/JS/tailwind.config.js
+++ Assets/JS/tailwind.config.js
@@ -2,6 +2,10 @@ tailwind.config = {
   theme: {
     extend: {
       colors: {
+        'figmabg1':'#0D0D0E',
+        'figmabg2':'#262627',
+        'figmabutton':'#D400B3',
+        'figmatext':'#E5E5E5',
         },
       },
   },
